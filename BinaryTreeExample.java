6060d6ed7e0756c7142dd712597bd7f82721b408
Suyash Marathe <115578928+SuyashMarathe@users.noreply.github.com>
2024-09-11 10:46:52 +0530

    BinaryTreeExample.java

--- BinaryTreeExample.java
+++ BinaryTreeExample.java
@@ -1,5 +1,4 @@
-
-public class BinaryTreeExample {
+  public class BinaryTreeExample {
     static class Node {
         int data;
         Node left;
@@ -26,10 +25,46 @@ public class BinaryTreeExample {
                return newNode;
             }
         }
+
+        //PREORDER TRAVERSAL OF BINARY TREE
+        public static void preOrder(Node root){
+        if(root==null){
+            return;
+        }
+            System.out.print(root.data+" ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+
+        //INORDER TRAVERSAL OF BINARY TREE
+      public static void inOrder(Node root){
+        if(root==null){
+            return;
+        }
+         inOrder(root.left);
+          System.out.print(root.data+" ");
+          inOrder(root.right);
+      }
+
+        //POSTORDER TRAVERSAL OF BINARY TREE
+      public static void postOrder(Node root){
+          if(root==null){
+              return;
+          }
+          postOrder(root.left);
+          postOrder(root.right);
+          System.out.print(root.data+" ");
+      }
+
     public static void main(String[] args) {
         int []nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
+        preOrder(root);
+        System.out.println();
+        inOrder(root);
+        System.out.println();
+        postOrder(root);
     }
 }
